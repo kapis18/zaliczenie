dd3116a6cea9592ab80b957724de7903372422ba
debek.kacper <215702@edu.p.lodz.pl>
2020-06-09 18:13:35 +0200

    Added behaviour test for correct programme behaviour in correct order

--- testsReactor/src/test/java/edu/iis/mto/testreactor/dishwasher/DishWasherTest.java
+++ testsReactor/src/test/java/edu/iis/mto/testreactor/dishwasher/DishWasherTest.java
@@ -9,7 +9,7 @@ import edu.iis.mto.testreactor.dishwasher.pump.PumpException;
 import edu.iis.mto.testreactor.dishwasher.pump.WaterPump;
 import org.junit.Before;
 import org.junit.Test;
-import org.mockito.Mock;
+import org.mockito.InOrder;
 import org.mockito.Mockito;
 
 public class DishWasherTest {
@@ -21,6 +21,7 @@ public class DishWasherTest {
     private DishWasher dishWasher;
     private final double correctFilterCapacity = 100d;
     private final double incorrectFilterCapacity = 30d;
+    private final int chosenProgrammeDurationMinutes = 90;
 
     @Before public void setUp() throws Exception {
         dishWasher = new DishWasher(waterPump, engine, dirtFilter, door);
@@ -29,8 +30,8 @@ public class DishWasherTest {
     @Test public void correctDishWashingShouldResultInSuccess() {
         when(door.closed()).thenReturn(true);
         when(dirtFilter.capacity()).thenReturn(correctFilterCapacity);
-        RunResult runResult = dishWasher.start(createProgramConfiguration());
-        RunResult expectedResult = RunResult.builder().withStatus(Status.SUCCESS).withRunMinutes(90).build();
+        RunResult runResult = dishWasher.start(createProgramConfiguration(true));
+        RunResult expectedResult = RunResult.builder().withStatus(Status.SUCCESS).withRunMinutes(chosenProgrammeDurationMinutes).build();
         assertEquals(expectedResult.getStatus(), runResult.getStatus());
         assertEquals(expectedResult.getRunMinutes(), runResult.getRunMinutes());
     }
@@ -38,48 +39,56 @@ public class DishWasherTest {
     @Test public void ifDoorIsOpenThenStatusShouldBeErrorDoorOpen() {
         when(door.closed()).thenReturn(false);
         when(dirtFilter.capacity()).thenReturn(correctFilterCapacity);
-        RunResult runResult = dishWasher.start(createProgramConfiguration());
-        RunResult expectedResult = RunResult.builder().withStatus(Status.DOOR_OPEN).withRunMinutes(90).build();
+        RunResult runResult = dishWasher.start(createProgramConfiguration(true));
+        RunResult expectedResult = RunResult.builder().withStatus(Status.DOOR_OPEN).withRunMinutes(chosenProgrammeDurationMinutes).build();
         assertEquals(expectedResult.getStatus(), runResult.getStatus());
     }
 
     @Test public void ifFilterIsNotCleanShouldReturnErrorFilter() {
         when(door.closed()).thenReturn(true);
         when(dirtFilter.capacity()).thenReturn(incorrectFilterCapacity);
-        RunResult runResult = dishWasher.start(createProgramConfiguration());
-        RunResult expectedResult = RunResult.builder().withStatus(Status.ERROR_FILTER).withRunMinutes(90).build();
+        RunResult runResult = dishWasher.start(createProgramConfiguration(true));
+        RunResult expectedResult = RunResult.builder().withStatus(Status.ERROR_FILTER).withRunMinutes(chosenProgrammeDurationMinutes).build();
         assertEquals(expectedResult.getStatus(), runResult.getStatus());
     }
 
     @Test public void filterShouldBeCleanIfTabletsNotUsed() {
         when(door.closed()).thenReturn(true);
         when(dirtFilter.capacity()).thenReturn(correctFilterCapacity);
-        dishWasher.start(ProgramConfiguration.builder()
-                                             .withFillLevel(FillLevel.FULL)
-                                             .withProgram(WashingProgram.ECO)
-                                             .withTabletsUsed(false)
-                                             .build());
+        dishWasher.start(createProgramConfiguration(false));
         verify(dirtFilter, never()).capacity();
     }
     @Test public void engineExceptionShouldResultInErrorProgram() throws EngineException {
         when(door.closed()).thenReturn(true);
         doThrow(EngineException.class).when(engine).runProgram(any());
         when(dirtFilter.capacity()).thenReturn(correctFilterCapacity);
-        RunResult runResult = dishWasher.start(createProgramConfiguration());
+        RunResult runResult = dishWasher.start(createProgramConfiguration(true));
         assertEquals(Status.ERROR_PROGRAM, runResult.getStatus());
     }
     @Test public void pumpExceptionShouldResultInErrorPump() throws PumpException {
         when(door.closed()).thenReturn(true);
         doThrow(PumpException.class).when(waterPump).drain();
         when(dirtFilter.capacity()).thenReturn(correctFilterCapacity);
-        RunResult runResult = dishWasher.start(createProgramConfiguration());
+        RunResult runResult = dishWasher.start(createProgramConfiguration(true));
         assertEquals(Status.ERROR_PUMP, runResult.getStatus());
     }
-    private ProgramConfiguration createProgramConfiguration() {
+    @Test public void correctWashingShouldCallDoorAndWaterPumpAndEngineInCorrectOrder() throws PumpException, EngineException {
+        when(door.closed()).thenReturn(true);
+        when(dirtFilter.capacity()).thenReturn(correctFilterCapacity);
+        dishWasher.start(createProgramConfiguration(true));
+        InOrder inOrder = inOrder(door, waterPump, engine);
+        inOrder.verify(door).closed();
+        inOrder.verify(door).lock();
+        inOrder.verify(waterPump).pour(any());
+        inOrder.verify(engine).runProgram(any());
+        inOrder.verify(waterPump).drain();
+    }
+    private ProgramConfiguration createProgramConfiguration(boolean tabletsUsed) {
         return ProgramConfiguration.builder()
                                    .withFillLevel(FillLevel.FULL)
                                    .withProgram(WashingProgram.ECO)
-                                   .withTabletsUsed(true)
+                                   .withTabletsUsed(tabletsUsed)
                                    .build();
     }
+
 }
