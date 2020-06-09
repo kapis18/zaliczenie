b639d0475e17e9bedb5c5d93a851fb147fc23bd8
debek.kacper <215702@edu.p.lodz.pl>
2020-06-09 17:29:37 +0200

    Created mocks and setUp method.

--- testsReactor/src/test/java/edu/iis/mto/testreactor/dishwasher/DishWasherTest.java
+++ testsReactor/src/test/java/edu/iis/mto/testreactor/dishwasher/DishWasherTest.java
@@ -2,11 +2,24 @@ package edu.iis.mto.testreactor.dishwasher;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 
+import edu.iis.mto.testreactor.dishwasher.engine.Engine;
+import edu.iis.mto.testreactor.dishwasher.pump.WaterPump;
 import org.hamcrest.Matchers;
-import org.junit.jupiter.api.Test;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
 
 class DishWasherTest {
 
+    @Mock WaterPump waterPump;
+    @Mock Engine engine;
+    @Mock DirtFilter dirtFilter;
+    @Mock Door door;
+
+    private DishWasher dishWasher;
+    @Before public void setUp() throws Exception {
+        dishWasher = new DishWasher(waterPump, engine, dirtFilter, door);
+    }
     @Test
     public void itCompiles() {
         assertThat(true, Matchers.equalTo(true));
